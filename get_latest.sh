7b8cdb6e7f3ec16fa7722e1ca98ea9c0e78463e9
nwayyanant <nwayyananthlaing@gmail.com>
2025-09-07 23:51:59 +0800

    Update get_latest.sh

--- get_latest.sh
+++ get_latest.sh
@@ -2,12 +2,13 @@
 # get_latest.sh — Update the local repo from GitHub
 
 echo "[INFO] Updating repository"
-git pull "https://github.com/atl25/tipitaka-poc-starter.git"
+git pull "https://github.com/nwayyanant/tipitaka-poc-starter.git"
 
 if [ $? -ne 0 ]; then
   echo "[ERROR] git pull failed. Make sure this folder is a git repo cloned from the same URL."
-  echo "        If not, run: git clone https://github.com/atl25/tipitaka-poc-starter.git"
+  echo "        If not, run: git clone https://github.com/nwayyanant/tipitaka-poc-starter.git"
   exit 1
 fi
+
 
 echo "[OK] Repository is up to date."
